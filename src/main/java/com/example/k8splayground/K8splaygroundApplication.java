62e544dffc85c7a51f9a33cffd559d65a9b05778
Julian David Bedoya Tafur <jbedoya@bancolombia.com.co>
2026-05-28 10:19:56 -0500

    initial proyect structure

---
+++ src/main/java/com/example/k8splayground/K8splaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8splayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8splaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8splaygroundApplication.class, args);
+	}
+
+}
